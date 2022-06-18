9ce16044ab1c6a24324e4fc46614938e4b16a4d5
starostinanatalie <starostina.natalie@gmail.com>
2022-06-18 12:48:14 +0300

    second commit

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-import swapi
+# import swapi
 import requests
-from PIL import Image
+# from PIL import Image
 import webbrowser
+from datetime import datetime
 
-
-res = requests.get('https://scotch.io/life')
+res = requests.get('https://scotch.io')
 
 print(res)
 
 image_url = 'https://http.cat/' + str(res.status_code)
 
 print(image_url)
-webbrowser.open(image_url)
+# webbrowser.open(image_url)
+
+# city_request = requests.get(f'https://www.metaweather.com/api/location/search/?query={city}')
+# if city_request.status_code == 200:
+#     city_data = city_request.json()
+#     city_id = city_data[0]['woeid']
+# else:
+#     print("Huston, we have a problem")
+
 
 # with Image.open('C:\Users\starostina\Pictures\Boris-A2-Deutch.bmp') as img:
 #     img.show()
 
-swapi_res = requests.get('https://swapi.dev')
-
-print(swapi_res)
+swapi_res = requests.get('https://swapi.dev/api/planets/1')
+swapi_data = swapi_res.json()
+print(swapi_data)
 
 # pm = swapi.get_film(4)
 # jj = swapi.get_person(36)
